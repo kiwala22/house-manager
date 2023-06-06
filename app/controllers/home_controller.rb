79d89acff36dd450b6427ed399a7a00afbc306c2
martin kiwala <martinkiwala4@gmail.com>
2023-06-06 11:14:57 -0500

    fix: add comment in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # this will be the index page to retrieve data
+    # TODO: e_e remove this comment
   end
 end
