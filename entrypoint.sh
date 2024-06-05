af4f1507f6350e08122a9e000d1ca260997ed755
martin kiwala <martinkiwala4@gmail.com>
2024-06-05 10:52:09 +0300

    added comment to check docker network

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ rm -f ./tmp/pids/server.pid
 # Make sure db is ready to go
 # Adding '2>/dev/null' sends output to nowhere in the case of an error and the
 # error code also triggers the bash OR to run db:setup
+echo "This is running database setups....."
 bundle exec rails db:migrate 2>/dev/null || bundle exec rails db:prepare
 
 # Then exec the container's main process (CMD in the Dockerfile).
